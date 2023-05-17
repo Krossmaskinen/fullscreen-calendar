193f156b870f0c166bf4379ffe7fedeab65965e6
Bill Kurtson <bill.kurtson@gmail.com>
2023-05-17 10:27:04 +0200

    add dialog for adding events

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -5,6 +5,13 @@
 	let monthData;
 	let selectedDay;
 	let showDayDetails = false;
+	let showAddEvent = false;
+
+	let eventTitle = '';
+	let eventStart = '';
+	let eventEnd = '';
+	let eventDescription = '';
+	let eventName = '';
 
 	const selectDay = (day) => {
 		selectedDay = day;
@@ -13,6 +20,30 @@
 
 	const goBack = () => {
 		showDayDetails = false;
+	};
+
+	const openAddEvent = () => {
+		showAddEvent = true;
+	};
+
+	const closeAddEvent = () => {
+		showAddEvent = false;
+		eventTitle = '';
+		eventStart = '';
+		eventEnd = '';
+		eventDescription = '';
+		eventName = '';
+	};
+
+	const saveEvent = () => {
+		selectedDay.event = {
+			title: eventTitle,
+			start: eventStart,
+			end: eventEnd,
+			description: eventDescription,
+			name: eventName
+		};
+		closeAddEvent();
 	};
 
 	const updateMonth = (newDate) => {
@@ -30,6 +61,27 @@
 		<button on:click={goBack}>Back</button>
 		<h2>Selected day: {selectedDay.date.toDateString()}</h2>
 		<p>{selectedDay.details}</p>
+		{#if selectedDay.event}
+			<div>
+				<h3>{selectedDay.event.title}</h3>
+				<p>{selectedDay.event.start} - {selectedDay.event.end}</p>
+				<p>{selectedDay.event.description}</p>
+				<p>{selectedDay.event.name}</p>
+			</div>
+		{/if}
+		<button on:click={openAddEvent}>Add Event</button>
+		{#if showAddEvent}
+			<div class="dialog">
+				<h3>Add Event</h3>
+				<input bind:value={eventTitle} placeholder="Title" />
+				<input type="time" bind:value={eventStart} placeholder="Start Time" />
+				<input type="time" bind:value={eventEnd} placeholder="End Time" />
+				<textarea bind:value={eventDescription} placeholder="Description" />
+				<input bind:value={eventName} placeholder="Name (Optional)" />
+				<button on:click={closeAddEvent}>Close</button>
+				<button on:click={saveEvent}>Save</button>
+			</div>
+		{/if}
 	</div>
 {:else}
 	<div>
@@ -46,6 +98,12 @@
 				<div class="day" on:click={() => selectDay(day)}>
 					<h3>{day.date.getDate()}</h3>
 					<p>{day.details}</p>
+					{#if day.event}
+						<div>
+							<h4>{day.event.title}</h4>
+							<p>{day.event.start} - {day.event.end}</p>
+						</div>
+					{/if}
 				</div>
 			{/each}
 		</div>
@@ -72,5 +130,12 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+	}
+	.dialog {
+		position: absolute;
+		width: 300px;
+		padding: 20px;
+		background: white;
+		border: 1px solid #ccc;
 	}
 </style>
